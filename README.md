83409d41a0ec345e5dc6e8180a98edf12c7610de
Ayoush Chourasia <ayoushchourasia1418@gmail.com>
2024-05-20 10:23:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # medium-followers-count
-Fetch your followers count from MEDIUM
+Fetch your followers count as well as your clap count from MEDIUM
+
+
+This sample code scraps your profile page in MEDIUM and pulls out your followers count and if you use other function and pass your post url it will pull out the clap count since MEDIUM doens't provide a REST API to do the same.
+
+
+## NOTE
+
+IN CLAP COUNT API I WAS GETTING CORS ERROR AND DIDN'T THOUGHT ABOUT THE WORK AROUND TO THIS BUT IF YOU  FIND ONE FEEL FREE TO RAISE A PR.
---
+++ api/index.js
@@ -0,0 +1,79 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors())
+
+async function getMediumStoryStats(storyUrl) {
+    if (!storyUrl.includes("medium.com")) {
+        throw new Error("Not authorized");
+    } else {
+        try {
+            const response = await axios.get(storyUrl);
+            const html = response.data;
+            // const $ = cheerio.load(html);
+            const clapsIndex = html.indexOf("clapCount\":") + "clapCount\":".length;
+            const endIndex = html.indexOf(",", clapsIndex);
+            const claps = parseInt(html.substring(clapsIndex, endIndex));
+            // const comments = parseInt($('.pw-responses-count').first().text().trim());
+            return claps;
+        } catch (error) {
+            console.error("Error:", error);
+            throw new Error("Internal server error");
+        }
+    }
+}
+
+
+async function scrapeFollowersCount(profileUrl) {
+    try {
+        const response = await axios.get(profileUrl);
+        
+        const $ = cheerio.load(response.data);
+        
+        const followersElement = $('span.pw-follower-count.be.b.fx.ga.dn');
+        
+        const followersText = followersElement.text().trim();
+        
+        const followersCount = parseInt(followersText.match(/\d+/)[0]);
+
+        return followersCount;
+    } catch (error) {
+        console.error('An error occurred:', error.message);
+        throw new Error('Failed to scrape followers count');
+    }
+}
+
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+  
+
+app.get("/", async (req, res) => {
+    try {
+        const profileUrl = 'https://medium.com/@ayoushchourasia';
+        const followersCount = await scrapeFollowersCount(profileUrl);
+        res.status(200).json({ followersCount });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
